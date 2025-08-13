f67df9a2132d85de739e9cd5cb7757a87f4db8e1
Magomedaliev Alihan <ammagomed3@mts.ru>
2025-08-13 13:33:46 +0300

    new_commit

--- conftest.py
+++ conftest.py
@@ -1,14 +1,27 @@
+import os
+
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 import pytest
 from pages.project_page import ProjectPage
 
+
 @pytest.fixture(scope='function', autouse=True)
 def browser():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.close()
+    chrome_options = Options()
 
+    if os.getenv('CI'):
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    if not os.getenv('CI'):
+        driver.maximize_window()
+
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture()
